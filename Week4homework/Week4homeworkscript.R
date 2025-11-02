1ab5740cf32293e5c2576b83cf8dc81e9983af92
yxing82 <zczl459@ucl.ac.uk>
2025-11-02 18:20:20 +0000

    done fork and map

--- Week4homework/Week4homeworkscript.R
+++ Week4homework/Week4homeworkscript.R
@@ -5,9 +5,11 @@ library(sf)
 library(tidyverse)
 library(here)
 library(janitor)
+library(countrycode)
+library(tmap)
 
 #import and then inspect geojson file
-worldshape <- st_read("World_Countries_(Generalized)_9029012925078512962.geojson")
+worldshape <- st_read("World_Countries_(Generalized)_-573431906301700955")
 plot(worldshape)
 str(worldshape)
 
@@ -17,10 +19,9 @@ str(worldshape)
 inequalitydata <- read_csv("HDR25_Composite_indices_complete_time_series.csv",
                            na="NULL",
                            locale = locale(encoding = "latin1"))
-problems(inequalitydata)
 names(inequalitydata)
 head(inequalitydata)
-str(inequalitydata)
+
 #inequalitydata has 206 rows; 196-206 are groups/categories
 #problems seems to be showing that the columns I need are unexpected type; come back later
 #if issues
@@ -31,40 +32,26 @@ str(inequalitydata)
 
 giidata <- inequalitydata%>%
   clean_names()%>%
-  dplyr::select(iso3, country, gii_2019, gii_2010)
-str(giidata)
-giidata
+  dplyr::select(iso3, country, gii_2019, gii_2010) %>%
+  mutate(giiwithdiff = gii_2019-gii_2010)
 
 #make countrycodes in worldshape and giidata consistent
-
-install.packages("countrycode")
-library(countrycode)
-
-#work out how to use countrycode!
-?countrycode
-
 #create "CODE" column containing verified iso3 country codes for gii data
 
-giidataiso3 <- countrycode(sourcevar = giidata$country,
-                           origin = "country.name.en",
-                           destination = "iso3c",
-                           warn = TRUE,
-                           nomatch = NA,
-                           custom_dict = NULL,
-                           custom_match = NULL,
-                           origin_regex = NULL
-                           )
-giidataiso3
-#Warning Some values were not matched unambiguously: Arab States, East Asia and the Pacific, Europe and Central Asia, 
-#High human development, Latin America and the Caribbean, 
-#Low human development, Medium human development, South Asia, Sub-Saharan Africa, Very high human development, World
+giidataiso3 <- giidata %>%
+  mutate(iso_code = countrycode::countrycode(sourcevar = giidata$country,
+                                             origin = "country.name.en",
+                                             destination = "iso3c",
+                                             warn = TRUE,
+                                             nomatch = NA,
+                                             custom_dict = NULL,
+                                             custom_match = NULL,
+                                             origin_regex = NULL))
 
-giiwithiso3 <- giidata%>%
-  add_column(CODE = giidataiso3, .before = "country")
-giiwithiso3
 
 #do same for worldshape 
-worldshapeiso3 <- countrycode(sourcevar = worldshape$COUNTRY,
+worldshapeiso3 <- worldshape %>%
+  mutate(code = countrycode::countrycode(sourcevar = worldshape$COUNTRY,
                               origin = "country.name.en",
                               destination = "iso3c",
                               warn = TRUE,
@@ -72,60 +59,31 @@ worldshapeiso3 <- countrycode(sourcevar = worldshape$COUNTRY,
                               custom_dict = NULL,
                               custom_match = NULL,
                               origin_regex = NULL
-                              )
-worldshapeiso3
+                              ))
 
-#Received Warning message:
-  #Some values were not matched unambiguously: 
-#Azores, Bonaire, Canarias, Glorioso Islands, Juan De Nova Island, Madeira, Micronesia, Saba, Saint Eustatius, Saint Martin
-
-worldshapewithiso3 <- worldshape%>%
-  add_column(CODE = worldshapeiso3, .before = "COUNTRY")
-worldshapewithiso3
-
-#creating new column of difference in inequality between 2010 and 2019
-
-inequalitydiff <- giiwithiso3$gii_2019 - giiwithiso3$gii_2010
-inequalitydiff
-
-giiwithdiff <- giiwithiso3%>%
-  add_column(diff20192010 = inequalitydiff, .after = "country")
-giiwithdiff
 
 #join data. NB this is second (successful) way of doing so after first attempt
 # (commented out below) which tried to do so using new aligned ISO3 created
 # result with anomalous rows - due to islands?
 
-joineddata2 <- worldshapewithiso3%>%
+joineddata <- worldshapewithiso3%>%
   clean_names()%>%
   left_join(.,
-            giiwithdiff,
-            by = c("country" = "country"))
-     
-#tidy joineddata to remove duplicate columns (codes)
-
-Joineddatatidy <- joineddata2%>%
-  dplyr::select(fid, code, country, countryaff, aff_iso, diff20192010, gii_2019, gii_2010, geometry)
-Joineddatatidy  
+            giidataiso3,
+            by = c("code" = "iso_code"))
 
 
-#Joined by country instead of by iso codes (why the hint about countrycode?) because
-#attempting to join by ISO codes resulted in confusion and 351 rows due to mismatch in
-#how various territories are categorised.
-#Could this be solved by grouping?
-
-#Code commented out below:
-
-#join
-#joineddatacodes <- worldshapewithiso3%>%
-  #left_join(.,
-            #giiwithdiff,
-            #by = c("CODE"="CODE"))
-#joineddatacodes
-
-#Warning message for ref:
-#In sf_column %in% names(g) :
-  #Detected an unexpected many-to-many relationship between `x` and `y`.
-#ℹ Row 16 of `x` matches multiple rows in `y`.
-#ℹ Row 196 of `y` matches multiple rows in `x`.
-#ℹ If a many-to-many relationship is expected, set `relationship = "many-to-many"` to silence this warning.
+###############################################################################
+########### map the data ######################################################
+tmap_mode("plot")
+tm1 <- tm_shape(joineddata) +
+  tm_polygons("giiwithdiff",
+              col = "black",
+              lwd = 0.5,
+              lty = "dashed",
+              fill.chart = tm_chart_violin(),
+              fill.scale = tm_scale_intervals(
+                values = "brewer.bu_pu",   # this line control the color scale for a classed (binned) choropleth
+                n = 5,
+                style = "jenks"))
+tm1
